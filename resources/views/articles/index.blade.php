ea4552e69672c1cad95d272db61ba5e8ca68111a
Maija <hello@maijagrudule.com>
2018-02-13 14:02:59 +0100

    Layout file using Blade

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,26 +1,15 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends ('layout')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-
-    </head>
-    <body>
-
+@section ('content')
        <h1> Blog posts</h1>
-<ul>
-       @foreach ($articles as $article)
+        <ul>
+            @foreach ($articles as $article)
 
-      <li> <a href="/articles/{{$article ->id}}">{{ $article->title }} </a></li>
-      @endforeach
-    </ul>
-    </body>
-</html>
+            <li> <a href="/articles/{{$article ->id}}">{{ $article->title }} </a></li>
+            @endforeach
+          </ul>
+@endsection
+
+  @section ('footer')
+           footer
+@endsection
