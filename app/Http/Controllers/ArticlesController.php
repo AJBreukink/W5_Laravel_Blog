282932ad317ffdbf6c3b216f0268bca8cd642d17
Maija <hello@maijagrudule.com>
2018-02-15 15:49:11 +0100

    merge bug fix

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -24,4 +24,24 @@ class ArticlesController extends Controller
 
     public function create() {
 
+  return view('admin.form', ['categories' => Category::all()]);
+}
+public function store() {
+$this->validate(request(), [
+'title'=> 'required',
+'content'=> 'required'
+
+]);
+
+//creat a new article using request data
+$article = new Article;
+$article->title = request('title');
+$article->content = request('content');
+
+// save it to the database
+$article->save();
+
+//redirect to home page
+return redirect('/');
+}
 }
